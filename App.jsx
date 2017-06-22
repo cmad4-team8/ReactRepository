a937bce0b55b0edec2da930b2bfab1d1e2e4342b
praveen <pravsp@cisco.com>
2017-06-23 01:07:01 +0530

    Displaying Post Details

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (<div><h1>Hi</h1></div>);
+    }
+}
